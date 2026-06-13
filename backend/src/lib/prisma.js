0e45d64f347d0b5ef152c5b9e0e8e0aa856ef951
Sebastian_Simon <esnorbi8@gmail.com>
2026-06-12 19:37:41 -0500

    fix: usar DATABASE_URL en prisma.js

--- backend/src/lib/prisma.js
+++ backend/src/lib/prisma.js
@@ -2,7 +2,7 @@ const { PrismaClient } = require('../../generated/prisma')
 const { PrismaPg } = require('@prisma/adapter-pg')
 
 const adapter = new PrismaPg({
-  connectionString: 'postgresql://postgres:2550@localhost:5432/ferreteria_nasca',
+  connectionString: process.env.DATABASE_URL,
 })
 
 const prisma = new PrismaClient({ adapter })
