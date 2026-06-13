b4145a94ba6767cc898f5d225fdf6035ee0d480b
Sebastian_Simon <esnorbi8@gmail.com>
2026-06-12 19:44:35 -0500

    fix: simplificar conexion Prisma sin adaptador pg

--- backend/src/lib/prisma.js
+++ backend/src/lib/prisma.js
@@ -1,10 +1,11 @@
 const { PrismaClient } = require('../../generated/prisma')
-const { PrismaPg } = require('@prisma/adapter-pg')
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL
+    }
+  }
 })
-
-const prisma = new PrismaClient({ adapter })
 
 module.exports = prisma
